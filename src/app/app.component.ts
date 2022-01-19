0be3e3c2669deff84d0754789cc00c76e9d09c2a
Kike Ramírez <eramirez@barbaraiot.com>
2022-01-19 01:18:09 +0100

    Added typography support & framerate count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Font } from 'p5';
 import { P5JSInvoker } from 'src/app/p5-jsinvoker';
 
 @Component({
@@ -7,10 +8,11 @@ import { P5JSInvoker } from 'src/app/p5-jsinvoker';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent extends P5JSInvoker implements OnInit {
-  title = 'demosketch';
+  public title = 'demosketch';
   public x = -50;
   public y = 0;
   public radius = 10;
+  public font!: Font; fontsize!: number;
 
   constructor() {
     super();
@@ -19,13 +21,19 @@ export class AppComponent extends P5JSInvoker implements OnInit {
   ngOnInit(): void {
   }
 
+  preload(): void {
+    this.font = this.p5.loadFont('../assets/Roboto/Roboto-Regular.ttf');
+  }
+
   setup() {
     this.p5.createCanvas(this.p5.windowWidth, this.p5.windowHeight);
     this.y = this.p5.windowHeight * 0.5;
     this.radius = 30 + 70 * this.p5.random();
-    this.p5.noFill();
     this.p5.strokeWeight(2);
     this.p5.background(0);
+    this.fontsize = 0.08 * this.p5.windowHeight;
+    this.p5.textSize(this.fontsize);
+    this.p5.textFont(this.font);
   }
 
   draw() {
@@ -36,6 +44,28 @@ export class AppComponent extends P5JSInvoker implements OnInit {
       this.radius = 30 + 70 * this.p5.random();
     }
     this.p5.stroke(255, 100);
+    this.p5.noFill();
     this.p5.circle(this.x, this.y, this.radius);
+
+    this.p5.fill(255);
+    this.p5.stroke(255);
+    this.p5.textAlign('center', 'center');
+    this.p5.textSize(this.fontsize);
+    this.p5.text(this.title, 0.5 * this.p5.windowWidth, 0.06 * this.p5.windowHeight);
+
+    this.p5.textSize(0.03 * this.p5.windowHeight);
+    this.p5.fill(0);
+    this.p5.rect(0.9 * this.p5.windowWidth, 0.9 * this.p5.windowHeight, 0.1 * this.p5.windowWidth, 0.1 * this.p5.windowHeight);
+    this.p5.fill(255);
+    this.p5.textAlign('left', 'center');
+    this.p5.text(this.p5.int(this.p5.frameRate()), 0.94 * this.p5.windowWidth, 0.94 * this.p5.windowHeight);
+
+  }
+
+  windowResized(): void {
+    this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
+    this.p5.background(0);
+    this.fontsize = 0.1 * this.p5.windowHeight;
+    this.p5.textSize(this.fontsize);
   }
 }
--- src/app/p5-jsinvoker.ts
+++ src/app/p5-jsinvoker.ts
@@ -3,24 +3,35 @@ import * as p5 from 'p5';
 export abstract class P5JSInvoker {
   protected p5!: p5;
 
-  abstract setup() : void;
-  abstract draw() : void;
+  abstract preload(): void;
+  abstract setup(): void;
+  abstract draw(): void;
+  abstract windowResized(): void;
 
   protected startP5JS(containerElement: HTMLElement): void {
     this.p5 = new p5(this.generate_sketch(), containerElement);
   }
 
   private generate_sketch() {
-      const that = this;
+    const that = this;
 
-      return ((p: p5) => {
-        p.setup = function() {
-            that.setup();
-        };
+    return ((p: p5) => {
 
-        p.draw = function() {
-            that.draw();
-        };
-      })
+      p.preload = function() {
+        that.preload();
+      }
+
+      p.setup = function () {
+        that.setup();
+      };
+
+      p.draw = function () {
+        that.draw();
+      };
+
+      p.windowResized = function () {
+        that.windowResized();
+      }
+    })
   }
 }
